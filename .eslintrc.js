4e8b5cd038327528a44f7a91553bc5fee7e2d088
Troy Chryssos <troychryssos@gmail.com>
2020-07-24 10:28:26 -0400

    fixing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
 module.exports = {
+  env: {
+    "browser": true,
+    "react-native/react-native": true,
+    "es6": true,
+  },
   root: true,
-  extends: '@react-native-community',
-};
+  extends: [
+    "eslint:recommended",
+    '@react-native-community',
+    "plugin:react/recommended",
+    "airbnb",
+  ],
+  globals: {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    "ecmaFeatures": {
+        "jsx": true
+    },
+    "ecmaVersion": 11,
+    "sourceType": "module"
+  },
+  plugins: [
+    "react",
+    "react-native",
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    indent: [
+        "error",
+        "tab"
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    quotes: [
+        "error",
+        "single"
+    ],
+    semi: [
+        "error",
+        "never"
+    ]
+  }
+}
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      'root': ['./src'],
+      'extensions': ['.jsx', '.js'],
+      'alias': {
+        'components': './src/components',
+        'logic': './src/logic',
+        'pages': './src/pages',
+        'constants': './src/constants',
+        'static': './src/static'
+      }
+    }]
+  ],
 };
