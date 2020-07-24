4e137d4b05577d5042973d4be5aeedd1215406fa
Troy Chryssos <troychryssos@gmail.com>
2020-07-24 13:36:06 -0400

    adding audio toolkit again this time following instructions

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
 
 import AudioList from 'components/AudioList'
 
+const styles = StyleSheet.create({
+	app: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+})
+
 const App = () => (
-	<View>
+	<View style={styles.app}>
+		<Text h1>Audio</Text>
 		<AudioList />
 	</View>
 )
